6f26ccd743399b44345293f75245285e7d6a5ee9
Phodal Huang <h@phodal.com>
2024-02-02 14:29:04 +0800

    refactor: try format code again

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -8,25 +8,25 @@ import { initReactI18next } from 'react-i18next';
 const resources = {
   zh: {
     translation: {
-      AI4SDLC: "生成式 AI 赋能软件研发",
-      "OS Solution": "开源解决方案",
-      "Download White Paper": "下载《UnitMesh 技术白皮书》",
+      AI4SDLC: '生成式 AI 赋能软件研发',
+      'OS Solution': '开源解决方案',
+      'Download White Paper': '下载《UnitMesh 技术白皮书》',
       // 端到端的开源 AI 赋能软件研发全流程
-      "E2E open source AI4SDLC": "端到端的开源 AI 赋能软件研发全流程",
+      'E2E open source AI4SDLC': '端到端的开源 AI 赋能软件研发全流程',
       // 构建下一代软件生成式应用开发范式
-      "Build NG AI4SE": "构建下一代软件生成式应用开发范式",
+      'Build NG AI4SE': '构建下一代软件生成式应用开发范式',
       // AI 编码助手
-      "AI-Assistant Development": "AI 编码助手",
+      'AI-Assistant Development': 'AI 编码助手',
     },
   },
-  "en-US": {
+  'en-US': {
     translation: {
-      AI4SDLC: "AI Empowered Software Development",
-      "OS Solution": "Open Source Solution",
-      "Download White Paper": "Download White Paper of UnitMesh Technology",
-      "E2E open source AI4SDLC": "End-to-end open source AI4SDLC",
-      "Build NG AI4SE": "Build next-gen AI4SDLC",
-      "AI-Assistant Development": "AI-Assistant Development,
+      AI4SDLC: 'AI Empowered Software Development',
+      'OS Solution': 'Open Source Solution',
+      'Download White Paper': 'Download White Paper of UnitMesh Technology',
+      'E2E open source AI4SDLC': 'End-to-end open source AI4SDLC',
+      'Build NG AI4SE': 'Build next-gen AI4SDLC',
+      'AI-Assistant Development': 'AI-Assistant Development',
     },
   },
 };
@@ -44,12 +44,13 @@ i18n
       // if you're using a language detector, do not define the lng option
 
       interpolation: {
-        escapeValue: false // react already safes from xss
+        escapeValue: false, // react already safes from xss
       },
     },
     (err) => {
       if (err) return console.log('something went wrong loading', err);
       console.log('i18n loaded successfully');
+      return null;
     },
   );
 
